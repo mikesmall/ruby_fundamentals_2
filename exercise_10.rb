0489be29ccaefc54b4f0c423601923a9c97c319e
mikesmall <michael.l.a.small@gmail.com>
2017-05-17 18:22:11 -0400

    Exercise 10 completed

--- exercise_10.rb
+++ exercise_10.rb
@@ -30,3 +30,24 @@ puts " "
 # Use the keys method to output all of the cohort names.
 
 puts students.keys
+
+# Terminal line break between outputs
+puts " "
+
+# Increase each cohort size by 5% and display the new results.
+
+students.each do |cohort,class_size|
+  class_size = (class_size * 1.05).round
+  puts "Updated class size of #{cohort} is #{class_size}"
+end
+
+# Terminal line break between outputs
+puts " "
+
+# Delete the 2nd cohort and redisplay the hash.
+
+students.delete(:cohort2)
+# That feels like cheating, but I couldn't find a way to select by index number.
+students.each do |cohort,class_size|
+  puts "#{cohort}: #{class_size} students"
+end
